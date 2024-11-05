261170a86fd8d39106a41beab31f908901f1434d
Tafadzwa McNorman <gumbitafadzwa@gmail.com>
2024-11-05 14:12:19 +0200

    refactor: removed HTML and added rotating 3D cylinder in Canvas

--- basic/vite-react/src/App.jsx
+++ basic/vite-react/src/App.jsx
@@ -1,12 +1,37 @@
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { useRef } from 'react';
 
-import { Canvas } from '@react-three/fiber'
+const RotatingCube = () => { 
+  const meshRef = useRef();
 
-const App =()=>{
-  return(
-    <Canvas style={{height:'100vh', width:'100vw', display:'flex', justifyContent:'center', alignItems:'center'}}>
-      <h1>Hello Thrre.js</h1>
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+      meshRef.current.rotation.z += 0.01;
+    }
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <cylinderGeometry args={[1, 1, 1, 32]} />
+      <meshNormalMaterial color={'#468585'} emissive={'#468585'} />
+    </mesh>
+  );
+};
+
+const App = () => {
+  return (
+    <Canvas 
+      style={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      backgroundColor="#101010"
+    >
+      <OrbitControls enableZoom enablePan enableRotate />
+      <directionalLight position={[1, 1, 1]} intensity={1} color={'#ffffff'} />
+      <RotatingCube />
     </Canvas>
-  )
-}
+  );
+};
 
-export default App 
+export default App;
