763aeb8b6b09ce26b1597549fede6cb4e06dcf03
Tafadzwa McNorman <gumbitafadzwa@gmail.com>
2024-11-07 01:53:35 +0200

    Refactor Navbar component, add mobile menu toggle functionality, and delete unnecessary files

--- basic/vite-react/src/App.jsx
+++ basic/vite-react/src/App.jsx
@@ -1,38 +1,12 @@
-import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { useRef } from 'react';
-
-const RotatingCube = () => { 
-  const meshRef = useRef();
-
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += 0.01;
-      meshRef.current.rotation.y += 0.01;
-      meshRef.current.rotation.z += 0.01;
-    }
-  });
-
-  return (
-    <mesh ref={meshRef}>
-      <cylinderGeometry args={[1, 1, 1, 32]} />
-      <meshNormalMaterial color={'#468585'} emissive={'#468585'} />
-      <sparkles count={100} scale={10} size={6} speed={0.5} noise={0.5} color={'#9CDBA6'} />
-    </mesh>
-  );
-};
+import React from 'react';
+import Navbar from './sections/Navbar';
 
 const App = () => {
   return (
-    <Canvas 
-      style={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-      backgroundColor="#101010"
-    >
-      <OrbitControls enableZoom enablePan enableRotate />
-      <directionalLight position={[1, 1, 1]} intensity={1} color={'#ffffff'} />
-      <RotatingCube />
-    </Canvas>
+    <main className="text-3xl font-bold underline">
+      <Navbar />
+    </main>
   );
 };
 
-export default App;
+export default App; 
---
+++ basic/vite-react/src/constants/index.js
@@ -0,0 +1,165 @@
+export const navLinks = [
+    {
+      id: 1,
+      name: 'Home',
+      href: '#home',
+    },
+    {
+      id: 2,
+      name: 'About',
+      href: '#about',
+    },
+    {
+      id: 3,
+      name: 'Work',
+      href: '#work',
+    },
+    {
+      id: 4,
+      name: 'Contact',
+      href: '#contact',
+    },
+  ];
+  
+  export const clientReviews = [
+    {
+      id: 1,
+      name: 'Prosper Marinda',
+      position: 'Marketing Director at GreenLeaf',
+      img: 'assets/review1.png',
+      review:
+        'Working with Tafadzwa was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+    },
+    {
+      id: 2,
+      name: 'Vuanza Roger',
+      position: 'Founder of TechGear Shop',
+      img: 'assets/review2.png',
+      review:
+        'Tafadzwa’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+    },
+    {
+      id: 3,
+      name: 'John Dohsas',
+      position: 'Project Manager at UrbanTech ',
+      img: 'assets/review3.png',
+      review:
+        'I can’t say enough good things about Tafadzwa. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+    },
+    {
+      id: 4,
+      name: 'Changamire Dombo',
+      position: 'CEO of BrightStar Enterprises',
+      img: 'assets/review4.png',
+      review:
+        'Tafadzwa was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
+    },
+  ];
+  
+  export const myProjects = [
+    {
+      title: 'Cache Bank Business Landing Page',
+      desc: "Cache Bank's landing page offers tailored banking solutions for software developers. It features a hero section highlighting key offerings, a section showcasing the bank's main features, endorsements from users, and basic information about the bank. The page encourages visitors to join Cache Bank and participate in their Discord community.",
+      subdesc: 'Built with HTML, Tailwind CSS, and Google Fonts for custom typography and rapid, consistent UI design.',
+      href: 'https://github.com/TafadzwaTG/SDF_Portfolio_Piece_TAFMUZ559_FTO2403_GroupA1_TAFADZWA-MUZAMINDO_SDF11',
+      texture: '/textures/project/project1.mp4',
+      logo: '/assets/project-logo1.png',
+      logoStyle: {
+        backgroundColor: '#2A1816',
+        border: '0.2px solid #36201D',
+        boxShadow: '0px 0px 60px 0px #AA3C304D',
+      },
+      spotlight: '/assets/spotlight1.png',
+      tags: [
+        {
+          id: 1,
+          name: 'HTML',
+          path: '/assets/html.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'Google Fonts',
+          path: '/assets/google-fonts.svg',
+        },
+      ],
+    },
+    {
+      title: 'Kanban Task Management',
+      desc: 'This application is designed to help users manage tasks through a Kanban-style interface, organizing tasks into columns based on their status (TODO, DOING, DONE). The main components include a navigation sidebar, a header with options for adding and editing tasks, and modals for creating and editing tasks.',
+      subdesc: 'Built with HTML, CSS, Google Fonts for typography, and JavaScript for dynamic functions and interactions.',
+      href: 'https://github.com/TafadzwaTG/JSL_Portfolio_Piece_TAFMUZ559_FTO2403_GroupA1_Tafadzwa-Muzamindo_JSL11',
+      texture: '/textures/project/project2.mp4',
+      logo: '/assets/project-logo2.png',
+      logoStyle: {
+        backgroundColor: '#13202F',
+        border: '0.2px solid #17293E',
+        boxShadow: '0px 0px 60px 0px #2F6DB54D',
+      },
+      spotlight: '/assets/spotlight2.png',
+      tags: [
+        {
+          id: 1,
+          name: 'HTML',
+          path: '/assets/html.svg',
+        },
+        {
+          id: 2,
+          name: 'CSS',
+          path: 'assets/css.svg',
+        },
+        {
+          id: 3,
+          name: 'JavaScript',
+          path: '/assets/javascript.svg',
+        },
+      ],
+    },
+  ];
+  
+  
+  export const calculateSizes = (isSmall, isMobile, isTablet) => {
+    return {
+      deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+      deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+      cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+      reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+      ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+      targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+    };
+  };
+  
+  export const workExperiences = [
+    {
+      id: 1,
+      name: 'Freelance Projects',
+      pos: 'Web Developer',
+      duration: '2023 - Present',
+      title: 'I am currently working on freelance projects where I apply the skills I have learned in front-end development. This includes working with HTML, CSS, JavaScript, and frameworks like React and Tailwind to create responsive, user-friendly websites.',
+      icon: '/assets/freelance.svg',
+      animation: 'victory',
+    },
+    {
+      id: 2,
+      name: 'Omega Developers',
+      pos: 'Junior Web Developer',
+      duration: 'August 2024 - Present',
+      title: 'As a Junior Web Developer at Omega Developers, I contribute to the creation of websites for small local businesses, focusing on front-end development using modern web technologies.',
+      icon: '/assets/omega-developers.svg',
+      animation: 'clapping',
+    },
+    {
+      id: 3,
+      name: 'Self-Taught Learning',
+      pos: 'Web Developer Trainee',
+      duration: '2022 - 2023',
+      title: "I began my web development journey as a self-taught learner. Through online resources and hands-on projects, I gained proficiency in HTML, CSS, JavaScript, and React. My dedication to mastering web development led to my enrollment in a formal course.",
+      icon: '/assets/self-taught.svg',
+      animation: 'salute',
+    },
+  ];
+  
