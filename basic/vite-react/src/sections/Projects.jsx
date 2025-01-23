8528edcd325551a6d4d3d5762fd1e9a149d2b1e7
Tafadzwa McNorman <gumbitafadzwa@gmail.com>
2025-01-23 16:13:26 +0200

    feat: add Loading.jsx and DemoComputer.jsx components, install GSAP for animations

--- basic/vite-react/src/sections/Projects.jsx
+++ basic/vite-react/src/sections/Projects.jsx
@@ -1,5 +1,10 @@
-import { myProjects } from "../constants";
+import { Suspense } from "react";
 import { useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Center, OrbitControls } from "@react-three/drei";
+import CanvasLoader from '../components/Loading.jsx';
+import DemoComputer from '../components/DemoComputer.jsx';
+import { myProjects } from "../constants"; 
 
 const projectCount = myProjects.length;
 
@@ -60,7 +65,7 @@ const Projects = () => {
               target="_blank"
               rel="noreferrer"
             >
-              <p>Soure Code</p>
+              <p>Source Code</p>
               <img
                 src="/assets/arrow-up.png"
                 className="w-3 h-3"
@@ -90,6 +95,20 @@ const Projects = () => {
               />
             </button>
           </div>
+        </div>
+        <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
+          <Canvas>
+            <ambientLight intensity={Math.PI} />
+            <directionalLight position={[10, 10, 5]} />
+            <Center>
+              <Suspense fallback={<CanvasLoader />}>
+                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                  <DemoComputer texture={currentProject.texture} />
+                </group>
+              </Suspense>
+            </Center>
+            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} />
+          </Canvas>
         </div>
       </div>
     </section>
